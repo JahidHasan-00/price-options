1196f78a5ba0f68de209f2f138dcaa81449489dc
JahidHasan-00 <jahid.hasan50001@gmail.com>
2024-05-03 10:42:31 +0600

    Responsive Daisynav is added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import DaisyNav from './Components/DaisyNav/DaisyNav'
+function App() {
+
+  return (
+    <>
+      <DaisyNav></DaisyNav>
+      <h1 className='text-4xl bg-pink-700 p-2'>Price-Options</h1>
+    </>
+  )
+}
+
+export default App
